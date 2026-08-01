f67f1052b88d86ba20d61e51e2b4fe9738cf1b17
Ayush mahapatra <ayushmahapatra98@gmail.com>
2026-08-02 02:41:59 +0530

    update frontend API url to correct Render domain

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://verseshelf-backend.onrender.com/api",
+  baseURL: "https://verseshelf.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },

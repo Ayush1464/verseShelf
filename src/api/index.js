db7867e3f213a7be06c2cae17fa0f8186eb6ae4c
Ayush mahapatra <ayushmahapatra98@gmail.com>
2026-08-02 02:04:07 +0530

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-// Connect to Django default development port 8000
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json',
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
